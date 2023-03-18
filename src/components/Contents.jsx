d515e43b87c6af9b64a38d20be30fd3347921e61
Taofiq Sulayman <sulaymantaofiq@gmail.com>
2023-03-18 12:10:49 +0100

    fixes

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import creditCard from "../assets/card.png";
 import masterCardLogo from "../assets/mc_symbol.svg";
 import userImage from "../assets/profile-pic.png";
-import chart from "../assets/chart.png"
-import { ChevronDownIcon, PlusCircleIcon, ArrowUpOnSquareIcon, SquaresPlusIcon, TicketIcon, PencilIcon, MagnifyingGlassIcon, } from "@heroicons/react/24/outline";
+import chart from "../assets/chart.png";
+import {
+  ChevronDownIcon,
+  PlusCircleIcon,
+  ArrowUpOnSquareIcon,
+  SquaresPlusIcon,
+  TicketIcon,
+  PencilIcon,
+  MagnifyingGlassIcon,
+  ChatBubbleOvalLeftIcon
+} from "@heroicons/react/24/outline";
 
 import {
   ArrowTrendingUpIcon,
@@ -12,16 +21,14 @@ import {
   UsersIcon,
   BanknotesIcon,
   GiftIcon,
-  ClockIcon
+  ClockIcon,
 } from "@heroicons/react/24/outline";
-
-
 
 const Contents = () => {
   return (
     <div class="px-8 sm:ml-64 mt-14 h-full">
       <h1 className="font-bold mb-4">My Card</h1>
-      <div class="flex gap-8 mb-4 h-full">
+      <div class="flex flex-wrap gap-8 mb-4 h-full">
         <div class="flex flex-col gap-6 rounded h-full w-full lg:w-[300px] mr-4">
           <img
             className="lg:h-[125px] lg:w-full"
@@ -159,7 +166,7 @@ const Contents = () => {
         </div>
 
         <div class="flex flex-col gap-4 rounded-lg">
-          <div className="flex gap-8 ">
+          <div className="flex flex-wrap gap-8">
             <div className="flex flex-col gap-4">
               <div className="flex justify-between items-center w-[550px]">
                 <h2 className="font-semibold">Money Flow</h2>
@@ -187,10 +194,10 @@ const Contents = () => {
               </div>
 
               <div>
-                <img className="w-[550px]" src={chart} alt="chart" />
+                <img className="w-80 lg:w-[550px]" src={chart} alt="chart" />
               </div>
             </div>
-            <div className="w-[360px]">
+            <div className="w-[360px] flex flex-col gap-6">
               <div className="flex justify-between items-center">
                 <h2 className="font-semibold text-sm">Recent Contacts</h2>
                 <div className="flex gap-4">
@@ -210,11 +217,59 @@ const Contents = () => {
                 ))}
                 <ChevronRightIcon className="h-6 w-6 text-gray-500" />
               </div>
+
+              <div class="w-full p-6 flex flex-col gap-4 rounded-lg shadow-lg">
+                <h5 class="text-lg ">Group Party</h5>
+                <div className="flex justify-between items-center">
+                  <div className="flex gap-2">
+                    <div class="flex -space-x-2">
+                      <img
+                        class="w-8 h-8 border-1 border-white rounded-full dark:border-gray-800"
+                        src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+                        alt=""
+                      />
+
+                      <img
+                        class="w-8 h-8 border-1 border-white rounded-full dark:border-gray-800"
+                        src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+                        alt=""
+                      />
+
+                      <img
+                        class="w-8 h-8 border-1 border-white rounded-full dark:border-gray-800"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+                        alt=""
+                      />
+                    </div>
+                    <span class="flex items-center justify-center w-8 h-8 border-2 text-xs font-medium border-dotted border-[#0176FA] text-[#0176FA] rounded-full">
+                      +5
+                    </span>
+                  </div>
+                  <div class="relative">
+                    <ChatBubbleOvalLeftIcon className="h-6 w-6 text-gray-500" />
+                    <span class="top-0 left-4 absolute w-3.5 h-3.5 bg-[#0176FA] rounded-full"></span>
+                  </div>
+                </div>
+                <div class="flex justify-between items-center gap-8 pt-2">
+                  <div class="w-full rounded-lg lg:inline-flex gap-2 items-center justify-center py-2.5">
+                    <img
+                      className="w-9 h-9 rounded-full"
+                      src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                      alt=""
+                    />
+                    <div class="text-left">
+                      <div class="mb-1 text-sm font-semibold">Dakota Milk</div>
+                      <div class="-mt-1 text-xs">$420.00</div>
+                    </div>
+                  </div>
+                  <ChevronRightIcon className="h-4 w-4 text-gray-500" />
+                </div>
+              </div>
             </div>
           </div>
 
           <div className="w-full flex flex-col gap-8">
-            <div className="flex justify-between items-center">
+            <div className="flex gap-10 lg:justify-between items-center">
               <h2 className="font-semibold">Recent Transactions</h2>
               <div className="flex gap-2 items-center">
                 <p className="text-sm">View all</p>
@@ -222,7 +277,7 @@ const Contents = () => {
               </div>
             </div>
 
-            <div class="bg-white flex justify-between text-base overflow-hidden">
+            <div class="bg-white flex gap-2 lg:justify-between items-center text-base overflow-hidden">
               <div class="flex items-center">
                 <img
                   src="https://static.vecteezy.com/system/resources/previews/008/505/801/original/bitcoin-logo-color-illustration-png.png"
@@ -231,13 +286,13 @@ const Contents = () => {
                 />
                 <h2 class=" text-gray-900">Bitcoin Transactions</h2>
               </div>
-              <p>Jan 16, 2020</p>
-              <p>*****45242</p>
+              <p className="hidden lg:flex">Jan 16, 2020</p>
+              <p className="hidden lg:flex">*****45242</p>
               <p>-$853.00</p>
               <p className="text-[#61BB84] bg-[#EBFFF3] px-2 py-1">Success</p>
             </div>
 
-            <div class="bg-white flex justify-between text-base overflow-hidden">
+            <div class="bg-white flex gap-2 lg:justify-between items-center text-base overflow-hidden">
               <div class="flex items-center">
                 <img
                   src="https://media.istockphoto.com/id/1344327532/photo/studio-portrait-of-attractive-19-year-old-woman-with-brown-hair.jpg?b=1&s=170667a&w=0&k=20&c=qPtuG5hKcC8vva8y_HC9Xm5z31DeQG4LTI6RuSXJ6wE="
@@ -246,13 +301,13 @@ const Contents = () => {
                 />
                 <h2 class=" text-gray-900">Sent to antonia</h2>
               </div>
-              <p className="ml-8">Jan 16, 2020</p>
-              <p>*****87212</p>
+              <p className="ml-8 hidden lg:flex">Jan 16, 2020</p>
+              <p className="hidden lg:flex">*****87212</p>
               <p>-$153.00</p>
               <p className="text-[#FFAA90] bg-[#FFF1ED] px-2 py-1">Pending</p>
             </div>
 
-            <div class="bg-white flex justify-between text-base overflow-hidden">
+            <div class="bg-white flex gap-2 lg:justify-between items-center text-base overflow-hidden">
               <div class="flex items-center">
                 <img
                   src="https://banner2.cleanpng.com/20180418/xie/kisspng-paypal-logo-e-commerce-payment-system-paypal-5ad78137f1e9c2.9153968515240727599909.jpg"
@@ -261,8 +316,8 @@ const Contents = () => {
                 />
                 <h2 class=" text-gray-900">Withdraw Paypal</h2>
               </div>
-              <p className="ml-6">Jan 16, 2020</p>
-              <p>*****36275</p>
+              <p className="ml-6 hidden lg:flex">Jan 16, 2020</p>
+              <p className="hidden lg:flex">*****36275</p>
               <p>+$223.00</p>
               <p className="text-[#61BB84] bg-[#EBFFF3] px-2 py-1">Success</p>
             </div>
